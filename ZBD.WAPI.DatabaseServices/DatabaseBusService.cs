2e72cb274e104dfc42a0ecd2d7cd8c07340174c3
zntk <zntklol@gmail.com>
2018-06-18 18:33:04 +0200

    dodanie kilku projektów i zainstalowanie czegos do mongo db

---
+++ ZBD.WAPI.DatabaseServices/DatabaseBusService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZBD.WAPI.IServices;
+
+namespace ZBD.WAPI.DatabaseServices
+{
+    public class DatabaseBusService : IBusService
+    {
+        public IList<object> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
