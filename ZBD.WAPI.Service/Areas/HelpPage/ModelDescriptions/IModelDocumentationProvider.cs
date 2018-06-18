d50d8a9c6e9c079670ed7cd6d4ded1f6d3fb5a1c
zntk <zntklol@gmail.com>
2018-06-18 17:00:24 +0200

    Add project files.

---
+++ ZBD.WAPI.Service/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace ZBD.WAPI.Service.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
---
+++ ZBD.WAPI.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZBD.WAPI.Service.WebApiApplication" Language="C#" %>
